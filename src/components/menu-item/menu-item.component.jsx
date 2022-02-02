c5a19780621a9aa7ba644f5412f34c6a1d886cce
fikrinazilul1690 <fikrinazilul@gmail.com>
2022-02-02 11:56:38 +0700

    update menu-items to navigate

--- src/components/menu-item/menu-item.component.jsx
+++ src/components/menu-item/menu-item.component.jsx
@@ -1,10 +1,25 @@
 import React from "react";
+import { useLocation, useNavigate, useParams } from "react-router-dom";
 
 import "./menu-item.styles.scss";
 
-export const MenuItem = ({ title, imageUrl, size }) => {
+function withRouter(Component) {
+	function ComponentWithRouterProp(props) {
+		let location = useLocation();
+		let navigate = useNavigate();
+		let params = useParams();
+		return <Component {...props} router={{ location, navigate, params }} />;
+	}
+
+	return ComponentWithRouterProp;
+}
+
+const MenuItem = ({ title, imageUrl, size, router, linkUrl }) => {
 	return (
-		<div className={`${size} menu-item`}>
+		<div
+			className={`${size} menu-item`}
+			onClick={() => router.navigate(`${linkUrl}`)}
+		>
 			<div
 				className='background-image'
 				style={{ backgroundImage: `url(${imageUrl})` }}
@@ -16,3 +31,5 @@ export const MenuItem = ({ title, imageUrl, size }) => {
 		</div>
 	);
 };
+
+export default withRouter(MenuItem);
