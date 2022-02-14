13d3a861613a6bef2a0fd8d824824bf39caa6e52
fikrinazilul1690 <fikrinazilul@gmail.com>
2022-02-14 19:41:57 +0700

    change double quotes to single quotes

--- src/components/collection-item/collection-item.component.jsx
+++ src/components/collection-item/collection-item.component.jsx
@@ -1,9 +1,9 @@
-import React from "react";
-import { connect } from "react-redux";
-import { addItem } from "../../redux/cart/cart.actions";
-import { CustomButton } from "../custom-button/custom-button.component";
+import React from 'react';
+import { connect } from 'react-redux';
+import { addItem } from '../../redux/cart/cart.actions';
+import { CustomButton } from '../custom-button/custom-button.component';
 
-import "./collection-item.styles.scss";
+import './collection-item.styles.scss';
 
 const CollectionItem = ({ item, addItem }) => {
 	const { name, price, imageUrl } = item;
